0e8df4e8e6bdd092e29cc2b4de49cf02f398e40b
pranav-meshram-gl <80811452+pranav-meshram-gl@users.noreply.github.com>
2021-03-17 16:13:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'furniture_factory_project',
+    version = '1.0',
+    description = 'Furniture Factory project with factory app',
+    author = 'Pranav Meshram',
+    author_email = 'pranav.mesrham@globallogic.com',
+    packages = ['furniture_factory_project'],
+    install_requires = ['django = 2.2.5', 'djangorestframework = 3.13.2', 'pytz = 2021.1', 'sqlparse = 0.4.1'],
+    scripts=['manage.py'])
+)
