3f65a8b6e8d390d47a0cacc7fc8ac6a1018251e9
pranav-meshram-gl <80811452+pranav-meshram-gl@users.noreply.github.com>
2021-03-17 20:00:56 +0530

    Update views.py

--- factory_app/views.py
+++ factory_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
+from drf_multiple_model.viewsets import ObjectMultipleModelAPIViewSet
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,15 +13,11 @@ from factory_app.serializers import  TableLegSerializer, TableSerializer, LegSer
 # Create your views here.
 # Upon visiting the home page the the default basic urls for corresponding views via DefaultRouter will be presented.
 
-class TableLegModelViewSet(ModelViewSet):
-    queryset = TableLeg.objects.all()
-    serializer_class = TableLegSerializer
-
-
-class TableModelViewSet(ModelViewSet):
-    queryset = Table.objects.all()
-    serializer_class = TableSerializer
-
+class TableViewSet(ObjectMultipleModelAPIViewSet):
+    querylist = [
+        {'queryset': Table.objects.all(), 'serializer_class': TableSerializer},
+        {'queryset': TableLeg.objects.all(), 'serializer_class': TableLegSerializer},
+    ]
 
 class LegModelViewSet(ModelViewSet):
     queryset = Leg.objects.all()
