5b16f3792b439709f7d91305e888a81cbb24e446
pranav-meshram-gl <80811452+pranav-meshram-gl@users.noreply.github.com>
2021-03-17 16:14:39 +0530

    Delete admin.py

--- factory_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from factory_app.models import Table, Leg, Feet
-
-# Register your models here.
-
-admin.site.register(Table)
-admin.site.register(Leg)
-admin.site.register(Feet)
