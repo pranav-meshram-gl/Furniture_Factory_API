9e997bef7b8a71962dbc8998e74f34d6bdf8acd0
pranav-meshram-gl <80811452+pranav-meshram-gl@users.noreply.github.com>
2021-03-17 12:36:30 +0530

    Add files via upload
    Complete Furniture Factory project files

---
+++ factory_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from factory_app.models import Table, Leg, Feet
+
+# Register your models here.
+
+admin.site.register(Table)
+admin.site.register(Leg)
+admin.site.register(Feet)
---
+++ factory_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FactoryAppConfig(AppConfig):
+    name = 'factory_app'
